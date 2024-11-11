83ba89ffb7f905248ba31b3c39bc7ccd20f47e70
divij <divijghose@iisc.ac.in>
2024-11-11 18:36:13 +0530

    Added Division

--- example.py
+++ example.py
@@ -7,6 +7,12 @@ def subtract(x,y):
 def multiply(x,y):
     return x*y
 
+def divide(x,y):
+    if y == 0:
+        return "Error: Division by zero"
+    else:
+        return x/y
+
 print(add(3,4))
 
 print(subtract(3,4))
